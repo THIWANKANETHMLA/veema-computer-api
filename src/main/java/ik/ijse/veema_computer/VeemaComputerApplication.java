ff2e8be31a6f88a848c7c5c9e5c06ea8dc0b9f7c
Thiwanka <130285342+THIWANKANETHMLA@users.noreply.github.com>
2026-09-07 10:50:20 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/ik/ijse/veema_computer/VeemaComputerApplication.java
@@ -0,0 +1,13 @@
+package ik.ijse.veema_computer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeemaComputerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeemaComputerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ik/ijse/veema_computer/VeemaComputerApplicationTests.java
@@ -0,0 +1,13 @@
+package ik.ijse.veema_computer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeemaComputerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
