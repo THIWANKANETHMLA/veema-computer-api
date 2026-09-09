92a51b8313ab7177d883c951fc06ef5a4b3e5df6
Thiwanka <130285342+THIWANKANETHMLA@users.noreply.github.com>
2026-09-09 12:24:22 +0530

    role,user repo

---
+++ src/main/java/lk/ijse/veema_computer/repository/RoleRepository.java
@@ -0,0 +1,16 @@
+package lk.ijse.veema_computer.repository;
+
+import lk.ijse.veema_computer.entity.Role;
+import lk.ijse.veema_computer.enums.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByRoleName(RoleName roleName);
+
+    boolean existsByRoleName(RoleName roleName);
+}
